c8d00d4d9338d1fc698ccd00fc17ecc86213fe80
Wilson <wilsoncheong7405@gmail.com>
2024-06-16 16:09:18 -0400

    coded every piece's appearance, colors and shapes corrected

--- piece.js
+++ piece.js
@@ -20,11 +20,11 @@ class Piece {
 export class LPiece extends Piece {
   constructor() { // each specific piece should override this.
     super();
-    this.centerX = 7;
-    this.centerY = 4;
+    this.centerX = 9;
+    this.centerY = 1;
     this.orientation = "north";
     this.color = "orange"
-    /*
+    /*  
           .
       .[.].
     */
@@ -42,27 +42,147 @@ export class LPiece extends Piece {
 
 }
 
-class JPiece extends Piece {
+export class JPiece extends Piece {
+  constructor() { // each specific piece should override this.
+    super();
+    this.centerX = 3;
+    this.centerY = 11;
+    this.orientation = "north";
+    this.color = "blue"
+    /*  
+      .    
+      .[.].
+    */
+    this.offsets = [[-1,1],[-1,0],[0,0],[1,0]]
+  }
+
+  drawSelf(){
+    for (const[xOffset, yOffset] of this.offsets){
+      drawGridPiece(
+        this.centerY + yOffset,
+        this.centerX + xOffset,
+        this.color);
+    }
+  }
 
 }
 
-class TPiece extends Piece {
+export class TPiece extends Piece {
+  constructor() { // each specific piece should override this.
+    super();
+    this.centerX = 7;
+    this.centerY = 9;
+    this.orientation = "north";
+    this.color = "purple"
+    /*  
+        .    
+      .[.].
+    */
+    this.offsets = [[-1,0],[0,0],[0,1],[1,0]]
+  }
 
+  drawSelf(){
+    for (const[xOffset, yOffset] of this.offsets){
+      drawGridPiece(
+        this.centerY + yOffset,
+        this.centerX + xOffset,
+        this.color);
+    }
+  }
 }
 
-class OPiece extends Piece {
+export class OPiece extends Piece {
+  constructor() { // each specific piece should override this.
+    super();
+    this.centerX = 8;
+    this.centerY = 13;
+    this.orientation = "north";
+    this.color = "yellow"
+    /*  
+      . .   
+     [.].
+    */
+    this.offsets = [[0,1],[0,0],[1,0],[1,1]]
+  }
 
+  drawSelf(){
+    for (const[xOffset, yOffset] of this.offsets){
+      drawGridPiece(
+        this.centerY + yOffset,
+        this.centerX + xOffset,
+        this.color);
+    }
+  }
 }
 
-class IPiece extends Piece {
+export class IPiece extends Piece {
+  constructor() { // each specific piece should override this.
+    super();
+    this.centerX = 2;
+    this.centerY = 15;
+    this.orientation = "north";
+    this.color = "cyan"
+    /*         
+      .[.]. .
+    */
+    this.offsets = [[-1,0],[0,0],[1,0],[2,0]]
+  }
 
+  drawSelf(){
+    for (const[xOffset, yOffset] of this.offsets){
+      drawGridPiece(
+        this.centerY + yOffset,
+        this.centerX + xOffset,
+        this.color);
+    }
+  }
 }
 
-class SPiece extends Piece {
+export class SPiece extends Piece {
+  constructor() { // each specific piece should override this.
+    super();
+    this.centerX = 3;
+    this.centerY = 1;
+    this.orientation = "north";
+    this.color = "limeGreen"
+    /*  
+        . .  
+      .[.]
+    */
+    this.offsets = [[-1,0],[0,0],[0,1],[1,1]]
+  }
 
+  drawSelf(){
+    for (const[xOffset, yOffset] of this.offsets){
+      drawGridPiece(
+        this.centerY + yOffset,
+        this.centerX + xOffset,
+        this.color);
+    }
+  }
 }
 
 
-class ZPiece extends Piece {
+export class ZPiece extends Piece {
+  constructor() { // each specific piece should override this.
+    super();
+    this.centerX = 2;
+    this.centerY = 3;
+    this.orientation = "north";
+    this.color = "red"
+    /*  
+      . .  
+       [.].
+    */
+    this.offsets = [[-1,1],[0,1],[0,0],[1,0]]
+  }
 
+  drawSelf(){
+    for (const[xOffset, yOffset] of this.offsets){
+      drawGridPiece(
+        this.centerY + yOffset,
+        this.centerX + xOffset,
+        this.color);
+    }
+  }
 }
