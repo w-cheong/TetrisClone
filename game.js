b92d7c4ce47fd4f7e093837249f556d6ec755bbe
Wilson <wilsoncheong7405@gmail.com>
2024-07-18 22:55:08 -0400

    properly removed upward movement

--- game.js
+++ game.js
@@ -509,9 +509,9 @@ function keyUpHandler(e) {
     rightPressed = false;
   } else if (e.key === "Left" || e.key === "ArrowLeft") {
     leftPressed = false;
-  } else if (e.key === "Up" || e.key === "ArrowUp") {
+  } /* else if (e.key === "Up" || e.key === "ArrowUp") {
     upPressed = false;
-  } else if (e.key === "Down" || e.key === "ArrowDown") {
+  } */ else if (e.key === "Down" || e.key === "ArrowDown") {
     downPressed = false;
   }
 }
